cb8adb6e290d688ce17fe65b4d0566ecd93223f4
Andrea Battaglia <106253768+andreabattaglia-seo@users.noreply.github.com>
2022-06-13 11:02:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ import time
 #author_textrazor_token = os.getenv("TEXTRAZOR_TOKEN")
 author_textrazor_token = os.getenv("3c2d037d5a3f91e7842470c8d2457df2ed172ac59d4d9ace8b212e1a")
 #author_google_key = os.getenv("GOOGLE_KEY")
-author_google_key = os.getenv("GOOGLE_KEY")
+author_google_key = os.getenv("476c2d3b3685249858d6c774a59ab360a8eb8ae9")
 # print(author_google_key)
 
 st.set_page_config(
